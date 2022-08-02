6cfe23772d8ad9de6d9d1ddb5e95ec04ccc7af4b
cloudymike <mikael@cloudyforecast.com>
2022-08-01 21:00:44 -0700

    Initial deepsleep for 10 min

--- esp32/main.py
+++ esp32/main.py
@@ -10,30 +10,48 @@ import grafanaconfig
 import esp32
 import machine
 
+# Time between samples in seconds
+INTERVAL=600
+# Blue LED
+LED = machine.Pin (2, machine.Pin.OUT)
+
+
 if __name__ == "__main__":
 
+    # Turn on LED to indicate it is awake.
+    LED.value(1)
 
+    # Connect to WiFi
     wlan.do_connect()
 
-    # Get the time
-    # NOT IF RESTART
-    #rtc = machine.RTC()
-    ntptime.settime()
-    POSIX_delta = 946684800 #ESP32 uses year 2000, linux 1970
+    if machine.reset_cause() != machine.DEEPSLEEP_RESET:
+        print('Power on or hard reset, set the time')
+        # NTP is timing out often. This may require retries
+        timeset = False
+        while not timeset:
+            try:
+                ntptime.settime()
+                timeset = True
+            except:
+                timeset = False
+                time.sleep(1)
+
+    POSIX_delta = 946684800 #ESP32 uses year 2000, linux 1970 for EPOCH
     timestamp = time.time() + POSIX_delta
     print("Timestamp: {}".format(timestamp))
 
     current_temp=esp32.raw_temperature()
     print("Temperature: {}".format(current_temp))
 
+
+    # Build the strings for HTTP request
     headers = {'content-type': 'application/json'}
-    INTERVAL=60
     actualtempdict = {"name": "actual_temperature", "interval": INTERVAL, "value": current_temp, "mtype":"gauge", "time": timestamp}
     mylist=[actualtempdict]
     data = json.dumps(mylist)
     print("Sending data {}".format(data))
 
-    #response = urequests.get('https://example.com')
+    # Send http post to grafana.net
     response = urequests.post(
         grafanaconfig.GRAFANA_URL,
         data=data,
@@ -42,3 +60,8 @@ if __name__ == "__main__":
     )
     status = response.status_code
     print("HTTP status code: {}".format(status))
+
+    # Turn off LED as we go do sleep
+    LED.value(0)
+    print("Going to deepsleep for {}s".format(INTERVAL))
+    machine.deepsleep(INTERVAL * 1000)
